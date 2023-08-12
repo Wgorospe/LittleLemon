d873b98f405dbced5b40218ecbe9b538b7a77c62
Wgorospe <weeestaaan@gmail.com>
2023-08-11 19:18:54 -0700

    Serializers and view classes added

--- littlelemonproject/restaurant/urls.py
+++ littlelemonproject/restaurant/urls.py
@@ -1,7 +1,10 @@
 #define URL route for index() view
 from django.urls import path
 from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleItemMenuView.as_view()),
 ]
--- littlelemonproject/restaurant/views.py
+++ littlelemonproject/restaurant/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from .serializers import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def post(self, request):
+        return render(request,'',{})
+    
+    def get(self,request):
+        return render(request,'',{})
+    
+
+class SingleItemMenuView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def post(self, request):
+        return render(request,'', {})
+    
+    def put(self, request):
+        return render(request,'',{})
+    
+    def delete(self, request):
+        return render(request,'',{})
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
